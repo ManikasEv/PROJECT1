454d33fb41c374a8f4c27a29113e679f1c9885f4
unknown <manikas329@gmail.com>
2021-07-17 21:23:27 +0300

    database

---
+++ user.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `user` (
+ `first name` varchar(100) ,
+ `last name` varchar(100) ,
+ `telephone` varchar (100),
+ `AFM`  int,
+ `AMKA` int,
+ `Address` varchar(200),
+ `ID` int,
+ `age` int,
+ `email` varchar (200),
+ `password` varchar(200),
+ 
+ PRIMARY KEY(`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
---
+++ vaccination_center.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `vaccination_center` (
+`name` varchar(200),
+`Address` varchar(200),
+`City` varchar(200),
+`vaccine type` varchar(200),
+`vaccine type2` varchar(200),
+ 
+ PRIMARY KEY(`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
